6d38ea8f388961cf111ac26cf4f7316c4f1ebeaf
Alexandre <164215444+al3xioux@users.noreply.github.com>
2026-03-04 09:40:06 +0100

    suppression forgotpassword

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import NewScan from "./pages/NewScan";
-import ScanInProgress from "./pages/ScanInProgress";
 import ScanResult from "./pages/ScanResult";
 import ScanHistory from "./pages/ScanHistory";
 import AuthPage from "./pages/AuthPage";
-import ForgotPassword from "./pages/ForgotPassword";
 import Profile from "./pages/Profile";
 
 const App = () => {
@@ -17,10 +15,8 @@ const App = () => {
         <Route path="/analyses/resultat" element={<ScanResult />} />
         <Route path="/analyses/historique" element={<ScanHistory />} />
         <Route path="/authpage" element={<AuthPage defaultView="login" />} />
-        <Route path="/mot-de-passe-oublie" element={<ForgotPassword />} />
         <Route path="/profile" element={<Profile />} />
-        {/* fallback */}
-        <Route path="*" element={<Navigate to="/analyses/nouveau" />} />
+        <Route path="*" element={<NewScan />} />
       </Routes>
     </Layout>
   );
